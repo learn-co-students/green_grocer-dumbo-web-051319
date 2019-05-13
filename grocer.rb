f99420c553e3aa4fe0c7c291ee2f902c5c45f5c3
Joe Jamsky <cba1067950@yahoo.com>
2019-05-13 19:42:26 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,35 @@
+require 'pry'
+
 def consolidate_cart(cart)
   # code here
+  newHash = [] 
+  quantity = 0
+  
+  #counts the number of items
+  cart.each_with_index do |item, index|
+    if newHash.include?(item) == false 
+      newHash.push(item)
+    else
+      #add to count 
+      newHash[index]
+      
+    end
+    binding.pry
+  end
+  binding.pry
+  
+  #cart.each do |product, product_hash|
+  #  if newHash.has_key?(product) == false
+      
+      #newHash = product_hash
+  ##    quantity += 1
+#    end
+ ##   binding.pry
+  #end
+  #assigns that number in a new value in the hash
+  #returns the new hash
+  
+  newHash
 end
 
 def apply_coupons(cart, coupons)
