470a0d96fd333e6e45d98f85268af2f3d1701503
Rachel R <rrrachelrath@gmail.com>
2019-05-25 17:52:57 -0400

    Done.

--- grocer.rb
+++ grocer.rb
@@ -28,28 +28,41 @@ end
   updated_cart
 end
 
+
 def apply_coupons(cart, coupons)
-  discounted_cart = {}
-    cart.each do |food, data|
-      coupons.each do |cheap_food|
-        if food == cheap_food[:item] && data[:count] >= cheap_food[:num]
-          data[:count] = data[:count] - cheap_food[:num]
-          binding.pry
-          if discounted_cart.key?("#{food} W/ COUPON")
-            discounted_cart["#{food} W/COUPON"][:count] += 1
-          else
-            discounted_cart["#{food} W/COUPON"] = {price: coupons[:cost], clearance: cart[:clearance]}
-        end
+  coupons.each do |coupon|
+    name = coupon[:item]
+    if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"]
+        cart["#{name} W/COUPON"][:count] += 1
+      else
+        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
       end
-      end
+      cart[name][:count] -= coupon[:num]
     end
-    discounted_cart
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |name, properties|
+    if properties[:clearance]
+      updated_price = properties[:price] * 0.80
+      properties[:price] = updated_price.round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
+  final_cart = apply_clearance(couponed_cart)
+  total = 0
+  final_cart.each do |name, properties|
+    total += properties[:price] * properties[:count]
+  end
+  total = total * 0.9 if total > 100
+  total
 end
